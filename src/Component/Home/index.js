6f7afccebf6fc3980c79fbb472dd25d5f16bac55
yuvi070 <yuvarajchavan448@gmail.com>
2024-10-20 03:26:23 +0000

    added input box and button

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -1,14 +1,50 @@
 import { Component } from "react";
 import Header from "../Header";
-
+import { v4 as uuidv4 } from "uuid";
 import "./index.css";
 
 class Home extends Component {
+  state = {
+    todoList: [],
+  };
+
+  componentDidMount() {
+    const existData = localStorage.getItem("myData");
+    this.setState({ todoList: existData });
+  }
+
   render() {
+    const { todoList } = this.state;
+    const myArray = ["Workout", "Study"];
+    const stringifiedArray = JSON.stringify(myArray);
+    localStorage.setItem("myData", stringifiedArray);
     return (
       <div className="home-main">
         <div className="body">
           <Header />
+          <div>
+            <form className="add-task-container">
+              <input
+                type="text"
+                className="input-element"
+                placeholder="Add Task"
+              />
+              <button type="submit" className="add-task-button">
+                Add
+              </button>
+            </form>
+          </div>
+          <h1>Tasks:</h1>
+          <ul className="todo-list-container">
+            <li>
+              <div className="todo-item">
+                <p>Workout</p>
+                <button type="button">
+                  <input type="checkbox" />
+                </button>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     );
@@ -16,3 +52,5 @@ class Home extends Component {
 }
 
 export default Home;
+
+// required object keys are id(uuid),name,isCompleted,timeOfCreation,
